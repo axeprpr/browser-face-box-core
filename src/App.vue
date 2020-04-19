0dd0e5fafdb8d82fc660ed1f84885c2061cd4bf1
Axe.Xu <axe3@qq.com>
2020-04-19 15:54:16 +0800

    chore(ui): simplify app shell and remove scaffold noise

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+    <h1 class="title">Browser Face Detection</h1>
+    <HelloWorld />
+  </main>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'AppRoot',
   components: {
     HelloWorld
   }
@@ -18,11 +18,18 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  padding: 24px 12px;
+  box-sizing: border-box;
+}
+.title {
+  margin: 0 0 16px;
+  font-size: 24px;
+  font-weight: 600;
 }
 </style>
